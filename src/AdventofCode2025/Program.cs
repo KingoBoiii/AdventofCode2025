e73be8671ae9384e4635dbbf2f23ce8d4e98e8f4
KingoBoiii <kingodk@outlook.dk>
2025-12-01 23:31:31 +0100

    feat(day1): add solution for Day 1 puzzle
    Introduce logic to solve Day 1 of Advent of Code 2025:
    - Add `/assets/` folder and `Input_Day1.txt` for puzzle input.
    - Update `AdventofCode2025.csproj` to copy input file on build.
    - Replace default `Program.cs` logic to display puzzle solutions.
    - Add `Part1.cs` with methods to calculate the solution.
    - Implement dial rotation logic to count centers at `0`.

--- src/AdventofCode2025/Program.cs
+++ src/AdventofCode2025/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var examplePassword = Day1.Part1.GetExampleSolution();
+var password = Day1.Part1.GetSolution();
+
+Console.WriteLine($"Example Password: {examplePassword}");
+Console.WriteLine($"Password: {password}");
+
