d8c8506149679481aa0fe1c0af8094bf980da6bf
KingoBoiii <kingodk@outlook.dk>
2025-12-02 09:39:52 +0100

    refactor(day1): restrict PuzzleInputFilePath visibility
    Change `PuzzleInputFilePath` constant from `internal` to `private` to limit its accessibility to within the `Day1` class.

--- src/AdventofCode2025/Day1.cs
+++ src/AdventofCode2025/Day1.cs
@@ -2,7 +2,7 @@
 
 public static partial class Day1
 {
-    internal const string PuzzleInputFilePath = "assets/Input_Day1.txt";
+    private const string PuzzleInputFilePath = "assets/Input_Day1.txt";
 
     public static string GetPart1Solution()
     {
