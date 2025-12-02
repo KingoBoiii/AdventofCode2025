900a43c002217d30c4f77396996f51e2fa6b8e59
KingoBoiii <kingodk@outlook.dk>
2025-12-02 09:35:46 +0100

    refactor(tests): restructure Day1Tests class
    Refactor Day1Tests to make it a top-level sealed class.
    Remove unused `using Xunit.Sdk;` directive.
    Move `ExampleLines` property to top-level and reformat.
    Reformat test methods for consistency and readability.

--- tests/AdventofCode2025.Tests/Day1Tests.cs
+++ tests/AdventofCode2025.Tests/Day1Tests.cs
@@ -1,42 +1,38 @@
-﻿using Xunit.Sdk;
+﻿namespace AdventofCode2025.Tests;
 
-namespace AdventofCode2025.Tests
+public sealed class Day1Tests
 {
-    public class Day1Tests
+    public static IEnumerable<object[]> ExampleLines => new List<object[]>
     {
-        public static IEnumerable<object[]> ExampleLines => new List<object[]>
-        {
-            new object[] { new[] {
-                "L68",
-                "L30",
-                "R48",
-                "L5",
-                "R60",
-                "L55",
-                "L1",
-                "L99",
-                "R14",
-                "L82" } 
-            }
-
-        };
-
-        [Theory]
-        [MemberData(nameof(ExampleLines))]
-        public void Day1_Part1_Example_Password_ReturnsCorrectValue(string[] lines)
-        {
-            var examplePassword = Day1.GetPart1Password(lines);
-
-            Assert.Equal("3", examplePassword);
+        new object[] { new[] {
+            "L68",
+            "L30",
+            "R48",
+            "L5",
+            "R60",
+            "L55",
+            "L1",
+            "L99",
+            "R14",
+            "L82" } 
         }
+    };
 
-        [Theory]
-        [MemberData(nameof(ExampleLines))]
-        public void Day1_Part2_Example_Password_ReturnsCorrectValue(string[] lines)
-        {
-            var examplePassword = Day1.GetPart2Password(lines);
+    [Theory]
+    [MemberData(nameof(ExampleLines))]
+    public void Day1_Part1_Example_Password_ReturnsCorrectValue(string[] lines)
+    {
+        var examplePassword = Day1.GetPart1Password(lines);
 
-            Assert.Equal("6", examplePassword);
-        }
+        Assert.Equal("3", examplePassword);
+    }
+
+    [Theory]
+    [MemberData(nameof(ExampleLines))]
+    public void Day1_Part2_Example_Password_ReturnsCorrectValue(string[] lines)
+    {
+        var examplePassword = Day1.GetPart2Password(lines);
+
+        Assert.Equal("6", examplePassword);
     }
 }
