886f30a991e08e3a872f43ea40d015395327cefa
KingoBoiii <kingodk@outlook.dk>
2025-12-06 13:04:10 +0100

    refactor(output): standardize solution labels
    Standardize `Console.WriteLine` output labels across days.
    Replace custom labels with "Part 1 Solution" and "Part 2 Solution"
    for consistency and improved readability.

--- src/AdventofCode2025/Program.cs
+++ src/AdventofCode2025/Program.cs
@@ -9,8 +9,8 @@ Console.WriteLine("Advent of Code 2025 - Day 1");
 var day01Part1Solution = Day01.GetPart1Solution();
 var day01Part2Solution = Day01.GetPart2Solution();
 
-Console.WriteLine($"Password: {day01Part1Solution}");
-Console.WriteLine($"Password (method 0x434C49434B): {day01Part2Solution}");
+Console.WriteLine($"Part 1 Solution: {day01Part1Solution}");
+Console.WriteLine($"Part 2 Solution: {day01Part2Solution}");
 
 Console.WriteLine();
 Console.WriteLine("===================================");
@@ -21,8 +21,8 @@ Console.WriteLine("Advent of Code 2025 - Day 2");
 var day02Part1Solution = Day02.GetPart1Solution();
 var day02Part2Solution = Day02.GetPart2Solution();
 
-Console.WriteLine($"Invalid Product Id Sum: {day02Part1Solution}");
-Console.WriteLine($"Invalid Product Id Sum (2): {day02Part2Solution}");
+Console.WriteLine($"Part 1 Solution: {day02Part1Solution}");
+Console.WriteLine($"Part 2 Solution: {day02Part2Solution}");
 
 Console.WriteLine();
 Console.WriteLine("===================================");
@@ -33,8 +33,8 @@ Console.WriteLine("Advent of Code 2025 - Day 3");
 var day03Part1Solution = Day03.GetPart1Solution();
 var day03Part2Solution = Day03.GetPart2Solution();
 
-Console.WriteLine($"Largest Joltage Rating: {day03Part1Solution}");
-Console.WriteLine($"Largest Joltage Rating (Override): {day03Part2Solution}");
+Console.WriteLine($"Part 1 Solution: {day03Part1Solution}");
+Console.WriteLine($"Part 2 Solution: {day03Part2Solution}");
 
 Console.WriteLine();
 Console.WriteLine("===================================");
